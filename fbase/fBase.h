7fdb0292fd960498ed1a470b8e1a6d6c4d120e2f
H1dd3nM1nd <twomp021@gmail.com>
2021-01-15 16:44:17 +0100

    Fixed a couple things

--- fbase/fBase.h
+++ fbase/fBase.h
@@ -2,8 +2,32 @@
 #include <common.h>
 #include <actors.h>
 
+// Forward declaration
+class fBase_c;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+fBase_c *createChildObject(Actors name, fBase_c *parent, int settings, char unk);    // 80162C40
+fBase_c *createObject(Actors name, int settings, char unk);                          // 80162C60
+
+// Used by the above two. DO NOT USE.
+fBase_c *createChildUnderTree(Actors name, fBase_c *parent, int settings, char unk); // 80162BB0
+
+#ifdef __cplusplus
+}
+#endif
+
 class fBase_c {
 	public:
+
+		enum Basetype : u8 {
+			NONE = 0,
+			SCENE = 1,
+			ACTOR = 2,
+		};
+
 		u32 id;
 		u32 settings;
 		Actors name;
@@ -12,15 +36,15 @@ class fBase_c {
 		u8 wasDestroyed;
 		u8 wasCreatedInstantly;
 		u8 wasCreationDelayed;
-		u8 baseType;            // 0 = none, 1 = scene,  2 = actor
-		u8 currentProcessID;    // &1 = connect, &2 = create, &4 = execute, &8 = delete, &16 = draw
+		Basetype type;
+		u8 currentProcessID; // &1 = connect, &2 = create, &4 = execute, &8 = delete, &16 = draw
 
 		TreeNode link_connect;
 		OrderedLinkListEntry link_execute;
 		OrderedLinkListEntry link_draw;
 		LinkListEntry link_IDlookup;
 
-		void *unusedStruct;  // Checked by dtor, but never used
+		void *unusedStruct;  // Checked by dtor and afterDelete, but never used
 		void *unusedStruct2; // Checked by beforeDelete, but never used
 		u32 unk;
 		void *heap;
@@ -60,9 +84,6 @@ class fBase_c {
 		bool hasUninitialisedProcesses();           // 80162B60
 		fBase_c *findNextUninitialisedProcess();    // 80162AF0
 
-		static fBase_c *createChildObject(Actors name, fBase_c *parent, int settings, char unk);    // 80162C40
-		static fBase_c *createObject(Actors name, int settings, char unk);                          // 80162C60
-
 	private:
 		// Internal functions
 		int processOperation(PTMF beforeState, PTMF onState, PTMF afterState); // 80161E00
@@ -70,13 +91,12 @@ class fBase_c {
 		void doCreate();    // 80161F80
 		void doDelete();    // 80162160
 		void doExecute();   // 80162260
-		void doDelete();    // 80162360
+		void doDraw();      // 80162360
 		void doConnect();   // 80162420
 
-		// Used by the public createObject funcs
-		static fBase_c *createChildUnderTree(Actors name, fBase_c *parent, int settings, char unk); // 80162BB0
-		void runCreate();                                                                           // 80162A70
+		// Used by the createObject funcs
+		void runCreate(); // 80162A70
 
 		// Puts the settings into global vars
-		static void setNextState(Actors name, fBase_c *parent, int settings, char unk);             // 80162B90
+		static void setNextState(Actors name, fBase_c *parent, int settings, char unk); // 80162B90
 };
